cf4ddf1119383b4fd57787004c9151f0dad93ea0
Adams Adeyiga <adamsadeyiga1@gmail.com>
2023-09-06 12:13:34 +0100

    Add a command to get user input.

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 
 int main(int ac, char **argv, char **env)
 {
+	char *command, *prompt;
+	size_t len;
+
 	(void)ac;
 	(void)argv;
 	(void)env;
+	command = NULL;
+	prompt = "($) ";
+	len = 0;
 
-	printf("%s", "Simple Shell..");
+	printf("%s", prompt);
+	getline(&command, &len, stdin);
+	printf("%s", command);
 	return (0);
 }
