31981232d4b2780cbfc9a06c5bf0bd1884a47ada
Adams Adeyiga <adamsadeyiga1@gmail.com>
2023-09-06 11:36:22 +0100

    Create main.c and main.h files

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(int ac, char **argv, char **env)
+{
+	(void)ac;
+	(void)argv;
+	(void)env;
+
+	printf("%s", "Simple Shell..");
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
