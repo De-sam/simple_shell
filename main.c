b3395a715590b4ae9a82f1834ec87258bf17ae80
Adams Adeyiga <adamsadeyiga1@gmail.com>
2023-09-15 10:36:30 +0100

    EDIT: main.c file

--- main.c
+++ main.c
@@ -33,16 +33,31 @@ int main(int argc, char **argv)
 				{
 					const char *home_dir = getenv("HOME");
 					if (home_dir != NULL)
+					{
 						change_directory(home_dir);
+						for (i = 0; argv[i] != NULL; i++)
+							free(argv[i]);
+						free(argv);
+					}
 				}
 				else if (strcmp(argv[1], "-") == 0)
 				{
 					const char *prev_dir = getenv("OLDPWD");
 					if (prev_dir != NULL)
+					{
 						change_directory(prev_dir);
+						for (i = 0; argv[i] != NULL; i++)
+							free(argv[i]);
+						free(argv);
+					}
 				}
 				else
+				{
 					change_directory(argv[1]);
+					for (i = 0; argv[i] != NULL; i++)
+						free(argv[i]);
+					free(argv);
+				}
 			}
 			else
 				execmd(argv);
